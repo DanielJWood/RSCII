034e29d9cbb3c26185e6504c30d52835ffd40254
Daniel Wood <daniel.j.wood11@gmail.com>
2013-10-11 17:23:35 -0400

    first commit

---
+++ data.js
@@ -0,0 +1,83 @@
+var competition = [
+	{
+		"ID": "1",
+		"name": "Clean Energy States Alliance",
+		"city": "Montpelier",
+		"state": "VT",
+		"popimpact": "13,000,000",
+		"text": "The New England Solar Cost-Reduction Partnership will build a thriving regional solar market by: increasing coordination across Connecticut, Massachusetts, New Hampshire, Rhode Island, and Vermont; refining and deploying innovations developed in Connecticut and Massachusetts during Rooftop Solar Challenge I; and more widely implementing best practices across the region, including online permitting and group purchasing programs.",
+		"affected": ["CT","MA","NH","RI","VT"]
+	},
+	{
+		"ID": "2",
+		"name": "City University of New York",
+		"city": "New York",
+		"state": "NY",
+		"popimpact": "10,200,000",
+		"text": "NYSolar Smart will create a toolkit of policies, programs, and resources to enable jurisdictions, utilities, industry, and end users to reduce the time and cost of installing solar PV. These include Web-accessible permitting, virtual net-metering guidelines for master-metered buildings, group purchasing programs, and updated model zoning ordinances. NYSolar Smart IT tools will include a customizable customer acquisition portal, solar maps, market analytics, and a one-stop PV market portal.",
+		"affected": ["NY"]
+	},
+	{
+		"ID": "3",
+		"name": "Washington State Department of Commerce",
+		"city": "Olympia",
+		"state": "WA",
+		"popimpact": "10,500,000",
+		"text": "The Pacific Northwest Solar Partnership aims to double PV capacity in Washington and Oregon by creating a more uniform and vibrant regional solar market. The collaborative project will establish “solar ready” corridors and simple online permitting and common interconnection processes for 80% of residents. The effort will also continue to expand innovative, locally developed financing solutions such as community solar and \"Solarize\" programs to provide access to financing options in every utility territory in the two-state region. ",
+		"affected": ["OR","WA"]
+	},
+	{
+		"ID": "4",
+		"name": "Optony Inc.",
+		"city": "Santa Clara",
+		"state": "CA",
+		"popimpact": "6,000,0000",
+		"text": "The American Solar Transformation Initiative will use an innovative online Solar Roadmap platform and hands-on engagement to assist over 400 jurisdictions where solar potential is abundant, but resources and information are scarce. The project will improve permitting processes, establish solar-friendly planning and zoning guidelines, streamline the interconnection process, expand financing options, and ultimately develop strong solar markets across the country.",
+		"affected": ["CA","CO","DC","DE","FL","HI","IA","ID","MD","MO","NV","NY","PA","TX","UT","VA"]
+	},
+	{
+		"ID": "5",
+		"name": "Broward County, Florida",
+		"city": "Fort Lauderdale",
+		"state": "FL",
+		"popimpact": "4,000,000",
+		"text": "Go SOLAR–Florida will expand its market-leading online permitting solution to nine local municipalities and six additional Florida counties, and will continue to engage Florida stakeholders to expand financing options in the state as it unlocks its full potential as a leader in PV deployment.",
+		"affected": ["FL"]
+	},
+	{
+		"ID": "6",
+		"name": "Mid-America Regional Council (MARC)",
+		"city": "Kansas City",
+		"state": "MO",
+		"popimpact": "10,000,000",
+		"text": "MARC and the National Association of Regional Councils will use established and trusted relationships among regional planning councils and local governments to spread solar-friendly best practices to nine diverse regions across the country, building upon successful efforts in the Kansas City region to create a strong solar market.",
+		"affected": ["DC","FL","IN","KS","MO","NY","OH","PA","TX"]
+	},
+	{
+		"ID": "7",
+		"name": "Iowa Economic Development Authority",
+		"city": "Des Moines",
+		"state": "IA",
+		"popimpact": "3,100,000",
+		"text": "The Iowa Statewide Solar Readiness Initiative is a collaborative effort to promote solar energy adoption in Iowa. The Iowa Economic Development Authority and its partners will work with communities, utilities, and other solar stakeholders to develop educational materials, identify policy barriers and potential solutions, create an online solar toolbox, perform individual market barrier assessments for pilot communities, and create customized community action plans.",
+		"affected": ["IA"]
+	},
+	{
+		"ID": "8",
+		"name": "California Center for Sustainable Energy",
+		"city": "San Diego",
+		"state": "CA",
+		"popimpact": "37,000,000",
+		"text": "The Golden State Solar Impact project will transform California\'s solar market by making permitting and interconnection processes more uniform, rapid, and transparent across the state. The project will implement a standardized permitting process and develop tools such as a statewide interconnection and data portal to dramatically reduce soft costs in California.",
+		"affected": ["CA"]
+	},
+	{
+		"ID": "9",
+		"name": "Total",
+		"city": "Total",
+		"state": "CA",
+		"popimpact": "147,800,000",
+		"text": "Total",
+		"affected": ["CA","CO","CT","DC","DE","FL","HI","IA","ID","IN","KS","MA","MD","MO","NH","NV","NY","OH","OR","PA","RI","TX","UT","VA","VT","WA"]
+	}
+]
