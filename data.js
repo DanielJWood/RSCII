dc3885c225061dd20dcbfcc75642cb5d57f4f508
Daniel Wood <daniel.j.wood11@gmail.com>
2013-10-23 14:39:05 -0400

    updated data and css

--- data.js
+++ data.js
@@ -1,33 +1,15 @@
 var competition = [
 	{
 		"ID": "1",
-		"name": "Clean Energy States Alliance (CESA)",
-		"city": "Montpelier",
-		"state": "VT",
-		"popimpact": "13,000,000",
-		"text": "CESA's New England Solar Cost-Reduction Partnership will build a thriving regional solar market by: increasing coordination across Connecticut, Massachusetts, New Hampshire, Rhode Island, and Vermont; refining and deploying innovations developed in Connecticut and Massachusetts during Rooftop Solar Challenge I; and more widely implementing best practices across the region, including online permitting and group purchasing programs.",
-		"affected": ["CT","MA","NH","RI","VT"]
-	},
-	{
-		"ID": "2",
-		"name": "City University of New York (CUNY)",
-		"city": "New York",
-		"state": "NY",
-		"popimpact": "10,200,000",
-		"text": "CUNY's NYSolar Smart will create a toolkit of policies, programs, and resources to enable jurisdictions, utilities, industry, and end users to reduce the time and cost of installing solar photovoltaics (PV). These include Web-accessible permitting, virtual net-metering guidelines for master-metered buildings, group purchasing programs, and updated model zoning ordinances. NYSolar Smart IT tools will include a customizable customer acquisition portal, solar maps, market analytics, and a one-stop PV market portal.",
-		"affected": ["NY"]
-	},
-	{
-		"ID": "3",
 		"name": "Washington State Department of Commerce",
 		"city": "Olympia",
 		"state": "WA",
 		"popimpact": "10,500,000",
-		"text": "Led by the Washington State Department of Commerce, the Pacific Northwest Solar Partnership aims to double photovoltaic capacity in Washington and Oregon by creating a more uniform and vibrant regional solar market. The collaborative project will establish “solar ready” corridors and simple online permitting and common interconnection processes for 80% of residents. The effort will also continue to expand innovative, locally developed financing solutions such as community solar and \"Solarize\" programs to provide access to financing options in every utility territory in the two-state region. ",
+		"text": "Led by the Washington State Department of Commerce, the Pacific Northwest Solar Partnership aims to double photovoltaic capacity in Washington and Oregon. The project will establish “solar ready” corridors, simple online permitting and interconnection processes for 80% of residents. The effort will also continue to expand innovative, locally developed financing solutions such as community solar and \"Solarize\" programs to provide access to financing options in every utility territory in the two-state region. ",
 		"affected": ["OR","WA"]
 	},
 	{
-		"ID": "4",
+		"ID": "2",
 		"name": "Optony Inc.",
 		"city": "Santa Clara",
 		"state": "CA",
@@ -36,16 +18,7 @@ var competition = [
 		"affected": ["CA","CO","DC","DE","FL","HI","IA","ID","MD","MO","NV","NY","PA","TX","UT","VA"]
 	},
 	{
-		"ID": "5",
-		"name": "Broward County, Florida",
-		"city": "Fort Lauderdale",
-		"state": "FL",
-		"popimpact": "4,000,000",
-		"text": "Broward County's Go SOLAR–Florida will expand its market-leading online permitting solution to nine local municipalities and six additional Florida counties, and will continue to engage Florida stakeholders to expand financing options in the state as it unlocks its full potential as a leader in photovoltaic deployment.",
-		"affected": ["FL"]
-	},
-	{
-		"ID": "6",
+		"ID": "3",
 		"name": "Mid-America Regional Council (MARC)",
 		"city": "Kansas City",
 		"state": "MO",
@@ -54,7 +27,7 @@ var competition = [
 		"affected": ["DC","FL","IN","KS","MO","NY","OH","PA","TX"]
 	},
 	{
-		"ID": "7",
+		"ID": "4",
 		"name": "Iowa Economic Development Authority (IEDA)",
 		"city": "Des Moines",
 		"state": "IA",
@@ -63,13 +36,40 @@ var competition = [
 		"affected": ["IA"]
 	},
 	{
-		"ID": "8",
+		"ID": "5",
+		"name": "Clean Energy States Alliance (CESA)",
+		"city": "Montpelier",
+		"state": "VT",
+		"popimpact": "13,000,000",
+		"text": "CESA's New England Solar Cost-Reduction Partnership will build a thriving regional solar market by: increasing coordination across Connecticut, Massachusetts, New Hampshire, Rhode Island, and Vermont; refining and deploying innovations developed in Connecticut and Massachusetts during Rooftop Solar Challenge I; and more widely implementing best practices across the region, including online permitting and group purchasing programs.",
+		"affected": ["CT","MA","NH","RI","VT"]
+	},	
+	{
+		"ID": "6",
+		"name": "City University of New York (CUNY)",
+		"city": "New York",
+		"state": "NY",
+		"popimpact": "10,200,000",
+		"text": "CUNY's NYSolar Smart will create a toolkit of policies, programs, and resources to enable jurisdictions, utilities, industry, and end users to reduce the time and cost of installing solar photovoltaics (PV). These include Web-accessible permitting, virtual net-metering guidelines for master-metered buildings, group purchasing programs, and updated model zoning ordinances. NYSolar Smart IT tools will include a customizable customer acquisition portal, solar maps, market analytics, and a one-stop PV market portal.",
+		"affected": ["NY"]
+	},
+	{
+		"ID": "7",
 		"name": "California Center for Sustainable Energy (CCSE)",
 		"city": "San Diego",
 		"state": "CA",
 		"popimpact": "37,000,000",
 		"text": "CCSE's Golden State Solar Impact project will transform California\'s solar market by making permitting and interconnection processes more uniform, rapid, and transparent across the state. The project will implement a standardized permitting process and develop tools such as a statewide interconnection and data portal to dramatically reduce soft costs in California.",
 		"affected": ["CA"]
+	},
+	{
+		"ID": "8",
+		"name": "Broward County, Florida",
+		"city": "Fort Lauderdale",
+		"state": "FL",
+		"popimpact": "4,000,000",
+		"text": "Broward County's Go SOLAR–Florida will expand its market-leading online permitting solution to nine local municipalities and six additional Florida counties, and will continue to engage Florida stakeholders to expand financing options in the state as it unlocks its full potential as a leader in photovoltaic deployment.",
+		"affected": ["FL"]
 	},
 	{
 		"ID": "9",
